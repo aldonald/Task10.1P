72b3ac6e85d981668ce2f7406a8c685005c68dd8
Alex Donald <aldon80@gmail.com>
2020-10-02 08:59:37 +1000

    Added functionality to add requests

---
+++ server/models/request.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+
+const RequestSchema = new mongoose.Schema(
+  {
+    user:{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      unique: true
+    },
+    title: String,
+    description: String,
+    choiceQuestion: String,
+    choiceOptions: [String],
+    decisionTaskQuestion: String,
+    sentenceTaskQuestion: String,
+    masterWorkers: String,
+    reward: String,
+    workerNumbers: Number,
+    name: String,
+    logo: {
+      data: Buffer,
+      contentType: String
+    },
+    expiry: Date,
+    created: Date
+  }
+)
+
+module.exports = mongoose.model("Request", RequestSchema)

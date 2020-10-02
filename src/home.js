429a6dc01d9d7c75c5f718cf7805ce916db64d3f
Alex Donald <aldon80@gmail.com>
2020-10-02 10:35:58 +1000

    Changed unique constraint on db. Removed console logs.

--- src/home.js
+++ src/home.js
@@ -41,16 +41,16 @@ const Home = (props) => {
     <>
       <HeadImg />
       <h2 className="mt-3">Featured Requestors</h2>
-      <div className="container-fluid content-row mt-5">
-        <div className="row">
-          {requestorList.map((requestor, i) => (
-            <Requestor
-              key={`requestor-${i}`}
-              requestor={requestor}
-            />)
-          )}
-        </div>
-      </div>
+      <Container className="mt-5">
+        <Row>
+            {requestorList.map((requestor, i) => (
+              <Requestor
+                key={`requestor-${i}`}
+                requestor={requestor}
+              />)
+            )}
+        </Row>
+      </Container>
       <Footer />
     </>
   )
